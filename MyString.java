9ce47ce73898be948c518f9557365ce782fbf4a5
yairjanbar <yair.janbar@gmail.com>
2024-12-01 01:00:58 +0200

    change the space

--- MyString.java
+++ MyString.java
@@ -14,6 +14,19 @@ public class MyString {
         System.out.println(contains("personality", "son")); // true
         System.out.println(contains("personality", "dad")); // false
         System.out.println(contains("resignation", "sign")); // true
+
+        boolean test1 = MyString.contains("baba yaga", "baba");
+        boolean test2 = MyString.contains("baba yaga", "");
+        boolean test3 = !MyString.contains("baba yaga", "John Wick is the baba yaga");
+        boolean test4 = !MyString.contains("baba yaga", "Yaga");
+        boolean test5 = !MyString.contains("baba yaga", "babayaga");
+        System.out.println(test1);
+        System.out.println(test2);
+        System.out.println(test3);
+        System.out.println(test4);
+        System.out.println(test5);
+
+
     }
 
     /** Returns the lowercase version of the given string. */
@@ -37,6 +50,7 @@ public class MyString {
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
         boolean check=false;
+        if (str1==""||str2=="") return true;
         for (int i=0;i<str1.length();i++) {
             if (str1.charAt(i)==str2.charAt(0)){
                 check=true;
